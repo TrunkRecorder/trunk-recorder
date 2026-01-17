d475add3429875660d45caf7703f437916b05968
Luke Berndt <lukekb@gmail.com>
2026-01-17 10:38:05 -0500

    got FM Demod working Smartnet and switched over to that

--- trunk-recorder/systems/smartnet_fsk2_demod.cc
+++ trunk-recorder/systems/smartnet_fsk2_demod.cc
@@ -17,103 +17,49 @@ smartnet_fsk2_demod::smartnet_fsk2_demod(gr::msg_queue::sptr queue)
 
 smartnet_fsk2_demod::~smartnet_fsk2_demod() {
 }
-void smartnet_fsk2_demod::reset_block(gr::basic_block_sptr block) {
-  gr::block_detail_sptr detail;
-  gr::block_sptr grblock = cast_to_block_sptr(block);
-  detail = grblock->detail();
-  detail->reset_nitem_counters();
-}
-void smartnet_fsk2_demod::reset() {
-  /*reset_block(pll_freq_lock);
-  reset_block(pll_amp);
-  reset_block(noise_filter);
-  reset_block(sym_filter);
-  reset_block(fsk4_demod);*/
-  /*
-  pll_freq_lock->update_gains();
-  pll_freq_lock->frequency_limit();
-  pll_freq_lock->phase_wrap();
-  pll_demod->set_phase(0);*/
 
-  //  fsk4_demod->reset(); This one may have been working but removing for now to be safe
+void smartnet_fsk2_demod::reset() {
 }
 
 void smartnet_fsk2_demod::initialize() {
   const double channel_rate = symbol_rate * samples_per_symbol;
   const double pi = M_PI;
-  double samples_per_symbol = 5;
- double ntaps = 7 * samples_per_symbol;
 
- sym_taps = gr::filter::firdes::root_raised_cosine(1.0, channel_rate, symbol_rate, 0.35, ntaps);
-
-
+  // Baseband AGC
   baseband_amp = gr::op25_repeater::rmsagc_ff::make(0.01, 1.00);
 
-  // FSK4: Symbol Taps
-  double symbol_decim = 1;
-
+  // Symbol filter - simple averaging filter
+  std::vector<float> sym_taps;
   for (int i = 0; i < samples_per_symbol; i++) {
     sym_taps.push_back(1.0 / samples_per_symbol);
   }
-  sym_filter = gr::filter::fir_filter_fff::make(symbol_decim, sym_taps);
+  sym_filter = gr::filter::fir_filter_fff::make(1, sym_taps);
 
-  slicer = gr::digital::binary_slicer_fb::make( );
-  float gain_mu = 0.01;
-  float mu = 0.5;
-  float omega_relative_limit = 0.3;
-
-  pll_demod = gr::analog::pll_freqdet_cf::make(
-    2.0 / samples_per_symbol,
-    1 * pi / samples_per_symbol,
-    -1 * pi / samples_per_symbol);
-  fsk4_demod_mm = gr::digital::clock_recovery_mm_ff::make(samples_per_symbol,
-    0.25 * gain_mu * gain_mu,
-    mu,
-    gain_mu,
-    omega_relative_limit); //gr::digital::clock_recovery_mm_ff::make(samples_per_symbol, 0.1, 0.5, 0.05, 0.005);
+  // FSK4 demodulator
   fsk4_demod = gr::op25_repeater::fsk4_demod_ff::make(tune_queue, channel_rate, symbol_rate);
 
-  float fm_demod_gain = channel_rate / (2 * pi * symbol_rate);
+  // Binary slicer
+  slicer = gr::digital::binary_slicer_fb::make();
+
+  // FM demod gain based on deviation
+  // SmartNet uses ±1.2kHz deviation for FSK
+  const double deviation = 1200.0;
+  float fm_demod_gain = channel_rate / (2 * pi * deviation);
   fm_demod = gr::analog::quadrature_demod_cf::make(fm_demod_gain);
+
+  // Frame assembler and null sinks
   framer = gr::op25_repeater::frame_assembler::make("smartnet", 1, 1, rx_queue, false);
   null_sink1 = gr::blocks::null_sink::make(sizeof(uint16_t));
   null_sink2 = gr::blocks::null_sink::make(sizeof(uint16_t));
 
-  debug_sink_in = gr::blocks::file_sink::make(sizeof(gr_complex), "smartnet_debug_in.cfile");
-  debug_sink_sym = gr::blocks::file_sink::make(sizeof(float), "smartnet_debug_sym.float");
-  debug_sink_demod = gr::blocks::file_sink::make(sizeof(float), "smartnet_debug_demod.float");
-  
-
-  // This is the current approch in OP25, but they generate some of the filters differently
-  // connect(self(), 0, fm_demod,0);
-  // connect(fm_demod, 0, baseband_amp,0);
-  // connect(baseband_amp, 0, sym_filter, 0);
-  // connect(sym_filter, 0,fsk4_demod, 0);
-  // connect(fsk4_demod, 0, slicer, 0);
-  // connect(slicer, 0, framer, 0);
-
-  // connect(framer, 0, null_sink1, 0);
-  // connect(framer, 1, null_sink2, 0);
-
-    // Different debug sinks in case we want to look at the output at different stages
-    // connect(self(), 0, debug_sink_in, 0);
-    // connect(sym_filter, 0, debug_sink_sym, 0);
-    // connect(fsk4_demod, 0, debug_sink_demod, 0);
-
-
-
-  // This is the is the PLL approach and it seems to work
-  connect(self(), 0, pll_demod, 0);
-  connect(pll_demod, 0, fsk4_demod, 0);
+  // Signal flow: Input -> FM Demod -> Baseband AGC -> Symbol Filter -> FSK4 Demod -> Slicer -> Framer
+  connect(self(), 0, fm_demod, 0);
+  connect(fm_demod, 0, baseband_amp, 0);
+  connect(baseband_amp, 0, sym_filter, 0);
+  connect(sym_filter, 0, fsk4_demod, 0);
   connect(fsk4_demod, 0, slicer, 0);
   connect(slicer, 0, framer, 0);
 
   connect(framer, 0, null_sink1, 0);
   connect(framer, 1, null_sink2, 0);
-
-  // Different debug sinks in case we want to look at the output at different stages
-  // connect(self(), 0, debug_sink_in, 0);
-  // connect(pll_demod, 0, debug_sink_sym, 0);
-  // connect(fsk4_demod_mm, 0, debug_sink_demod, 0);
-
 }
--- trunk-recorder/systems/smartnet_fsk2_demod.h
+++ trunk-recorder/systems/smartnet_fsk2_demod.h
@@ -2,32 +2,22 @@
 #define SMARTNET_FSK2_DEMOD_H
 
 #include <boost/shared_ptr.hpp>
-#include <gnuradio/analog/pll_freqdet_cf.h>
 #include <gnuradio/analog/quadrature_demod_cf.h>
 #include <gnuradio/block.h>
-
-#include <gnuradio/block_detail.h>
-#include <gnuradio/filter/fft_filter_fff.h>
-#include <gnuradio/filter/firdes.h>
 #include <gnuradio/hier_block2.h>
 #include <gnuradio/io_signature.h>
 #include <gnuradio/msg_queue.h>
 #include <gnuradio/blocks/null_sink.h>
-#include <gnuradio/blocks/file_sink.h>
-#include <gnuradio/filter/fft_filter_ccf.h>
 
 #if GNURADIO_VERSION < 0x030800
-#include <gnuradio/blocks/multiply_const_ff.h>
 #include <gnuradio/filter/fir_filter_fff.h>
 #else
-#include <gnuradio/blocks/multiply_const.h>
 #include <gnuradio/filter/fir_filter_blk.h>
 #endif
-#include "../gr_blocks/rms_agc.h"
+
 #include <op25_repeater/rmsagc_ff.h>
 #include <op25_repeater/include/op25_repeater/fsk4_demod_ff.h>
 #include <op25_repeater/include/op25_repeater/frame_assembler.h>
-#include <gnuradio/digital/clock_recovery_mm_ff.h>
 #include <gnuradio/digital/binary_slicer_fb.h>
 
 
@@ -56,27 +46,14 @@ private:
   const int samples_per_symbol = 5;
   const double symbol_rate = 3600;
   gr::msg_queue::sptr rx_queue;
-  std::vector<float> baseband_noise_filter_taps;
-  std::vector<float> sym_taps;
   gr::msg_queue::sptr tune_queue;
-  std::vector<float> cutoff_filter_coeffs;
-  gr::filter::fft_filter_fff::sptr noise_filter;
   gr::filter::fir_filter_fff::sptr sym_filter;
-  gr::filter::fft_filter_ccf::sptr cutoff_filter;
-  gr::blocks::multiply_const_ff::sptr pll_amp;
-  gr::analog::pll_freqdet_cf::sptr pll_demod;
   gr::analog::quadrature_demod_cf::sptr fm_demod;
   gr::op25_repeater::rmsagc_ff::sptr baseband_amp;
   gr::op25_repeater::fsk4_demod_ff::sptr fsk4_demod;
-  gr::digital::clock_recovery_mm_ff::sptr fsk4_demod_mm;
-  gr::digital::clock_recovery_mm_ff::sptr clock_recovery;
   gr::digital::binary_slicer_fb::sptr slicer;
   gr::op25_repeater::frame_assembler::sptr framer;
   gr::blocks::null_sink::sptr null_sink1;
-  gr::blocks::null_sink::sptr null_sink2;
-  gr::blocks::file_sink::sptr debug_sink_in;
-  gr::blocks::file_sink::sptr debug_sink_sym;
-  gr::blocks::file_sink::sptr debug_sink_demod;
-  void reset_block(gr::basic_block_sptr block); 
+  gr::blocks::null_sink::sptr null_sink2; 
 };
 #endif
