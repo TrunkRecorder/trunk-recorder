d2922d3414be205c6905f13e9f5ee454880747cd
Luke Berndt <lukekb@gmail.com>
2026-01-17 14:21:16 -0500

    FSK4 matches OP25 FM Demod approach

--- trunk-recorder/recorders/p25_recorder_fsk4_demod.cc
+++ trunk-recorder/recorders/p25_recorder_fsk4_demod.cc
@@ -1,4 +1,5 @@
 #include "p25_recorder_fsk4_demod.h"
+#include <cmath>
 
 p25_recorder_fsk4_demod_sptr make_p25_recorder_fsk4_demod() {
   p25_recorder_fsk4_demod *recorder = new p25_recorder_fsk4_demod();
@@ -15,87 +16,120 @@ p25_recorder_fsk4_demod::p25_recorder_fsk4_demod()
 
 p25_recorder_fsk4_demod::~p25_recorder_fsk4_demod() {
 }
+
 void p25_recorder_fsk4_demod::reset_block(gr::basic_block_sptr block) {
   gr::block_detail_sptr detail;
   gr::block_sptr grblock = cast_to_block_sptr(block);
   detail = grblock->detail();
   detail->reset_nitem_counters();
 }
-void p25_recorder_fsk4_demod::reset() {
-  /*reset_block(pll_freq_lock);
-  reset_block(pll_amp);
-  reset_block(noise_filter);
-  reset_block(sym_filter);
-  reset_block(fsk4_demod);*/
-  /*
-  pll_freq_lock->update_gains();
-  pll_freq_lock->frequency_limit();
-  pll_freq_lock->phase_wrap();
-  pll_demod->set_phase(0);*/
 
-  //  fsk4_demod->reset(); This one may have been working but removing for now to be safe
+void p25_recorder_fsk4_demod::reset() {
+}
+
+/**
+ * Generate C4FM matched filter taps using the OP25 approach.
+ * This implements the P25 C4FM de-emphasis filter.
+ *
+ * These taps are pre-computed for sample_rate=24000, symbol_rate=4800, span=9
+ * using the OP25 algorithm: transfer_function_rx -> irfft -> fftshift -> window -> normalize
+ *
+ * The transfer function is: D(f) = sin(pi*f/rate) / (pi*f/rate) for f in [0, symbol_rate)
+ */
+std::vector<float> p25_recorder_fsk4_demod::generate_c4fm_taps(double sample_rate, double symbol_rate, int span) {
+  // Pre-computed C4FM taps for sample_rate=24000, symbol_rate=4800, span=9
+  // Generated using OP25's c4fm_taps algorithm with transfer_function_rx
+  static const float c4fm_taps_24000[] = {
+    4.0312432198e-04f,
+    -2.2037895551e-04f,
+    -6.3724152930e-04f,
+    -1.6166325335e-04f,
+    6.7865385656e-04f,
+    6.6618120420e-04f,
+    -4.0420558050e-04f,
+    -1.1383620250e-03f,
+    -2.6115800979e-04f,
+    1.3417428514e-03f,
+    1.3060668391e-03f,
+    -9.6070100493e-04f,
+    -2.5975062956e-03f,
+    -4.3705610688e-04f,
+    3.8328930923e-03f,
+    3.6392152810e-03f,
+    -4.3930370271e-03f,
+    -1.1280588508e-02f,
+    2.8130577702e-03f,
+    5.2971011626e-02f,
+    1.3150365306e-01f,
+    2.0542515300e-01f,
+    2.3582229080e-01f,
+    2.0542515300e-01f,
+    1.3150365306e-01f,
+    5.2971011626e-02f,
+    2.8130577702e-03f,
+    -1.1280588508e-02f,
+    -4.3930370271e-03f,
+    3.6392152810e-03f,
+    3.8328930923e-03f,
+    -4.3705610688e-04f,
+    -2.5975062956e-03f,
+    -9.6070100493e-04f,
+    1.3060668391e-03f,
+    1.3417428514e-03f,
+    -2.6115800979e-04f,
+    -1.1383620250e-03f,
+    -4.0420558050e-04f,
+    6.6618120420e-04f,
+    6.7865385656e-04f,
+    -1.6166325335e-04f,
+    -6.3724152930e-04f,
+    -2.2037895551e-04f,
+    4.0312432198e-04f
+  };
+
+  const int ntaps_24000 = 45;
+
+  // For the standard P25 Phase 1 rate, use pre-computed taps
+  if (std::abs(sample_rate - 24000.0) < 1.0 && std::abs(symbol_rate - 4800.0) < 1.0) {
+    return std::vector<float>(c4fm_taps_24000, c4fm_taps_24000 + ntaps_24000);
+  }
+
+  // Fallback: simple averaging filter for non-standard rates
+  int sps = static_cast<int>(sample_rate / symbol_rate);
+  std::vector<float> taps(sps);
+  float tap_value = 1.0f / sps;
+  for (int i = 0; i < sps; i++) {
+    taps[i] = tap_value;
+  }
+  return taps;
 }
 
 void p25_recorder_fsk4_demod::initialize() {
   const double phase1_channel_rate = phase1_symbol_rate * phase1_samples_per_symbol;
   const double pi = M_PI;
 
-  // FSK4: Phase Loop Lock - can only be Phase 1, so locking at that rate.
-  double freq_to_norm_radians = pi / (phase1_channel_rate / 2.0);
-  double fc = 0.0;
-  double fd = 600.0;
-  double pll_demod_gain = 1.0 / (fd * freq_to_norm_radians);
-  double samples_per_symbol = 5;
-  pll_freq_lock = gr::analog::pll_freqdet_cf::make((phase1_symbol_rate / 2.0 * 1.2) * freq_to_norm_radians, (fc + (3 * fd * 1.9)) * freq_to_norm_radians, (fc + (-3 * fd * 1.9)) * freq_to_norm_radians);
-  pll_amp = gr::blocks::multiply_const_ff::make(pll_demod_gain * 1.0);
+  // OP25 FSK4 Demodulation Chain:
+  // fm_demod -> baseband_amp -> sym_filter (C4FM) -> fsk4_demod
 
-  // FSK4: noise filter - can only be Phase 1, so locking at that rate.
-#if GNURADIO_VERSION < 0x030900
-  baseband_noise_filter_taps = gr::filter::firdes::low_pass_2(1.0, phase1_channel_rate, phase1_symbol_rate / 2.0 * 1.175, phase1_symbol_rate / 2.0 * 0.125, 20.0, gr::filter::firdes::WIN_KAISER, 6.76);
-#else
-  baseband_noise_filter_taps = gr::filter::firdes::low_pass_2(1.0, phase1_channel_rate, phase1_symbol_rate / 2.0 * 1.175, phase1_symbol_rate / 2.0 * 0.125, 20.0, gr::fft::window::WIN_KAISER, 6.76);
-
-#endif
-  noise_filter = gr::filter::fft_filter_fff::make(1.0, baseband_noise_filter_taps);
-
-  baseband_amp = gr::op25_repeater::rmsagc_ff::make(0.01, 1.00);
-
-  // FSK4: Symbol Taps
-  double symbol_decim = 1;
-
-  for (int i = 0; i < samples_per_symbol; i++) {
-    sym_taps.push_back(1.0 / samples_per_symbol);
-  }
-  sym_filter = gr::filter::fir_filter_fff::make(symbol_decim, sym_taps);
-
-  // FSK4: FSK4 Demod - locked at Phase 1 rates, since it can only be Phase 1
-  int omega = phase1_samples_per_symbol;
-  float gain_mu = 0.175;
-  float mu = 0.5;
-  float gain_omega = .25 * gain_mu * gain_mu;        //critically damped
-  float omega_relative_limit = 0.005;
-  clock_recovery = gr::digital::clock_recovery_mm_ff::make(omega, gain_omega,  mu, gain_mu, omega_relative_limit);
-  fsk4_demod = gr::op25_repeater::fsk4_demod_ff::make(tune_queue, phase1_channel_rate, phase1_symbol_rate);
-  int def_symbol_deviation = 600.0;
-  float fm_demod_gain = phase1_channel_rate / (2 * pi * def_symbol_deviation);
+  // FM Demodulator: gain = sample_rate / (2 * pi * symbol_deviation)
+  const double def_symbol_deviation = 600.0;
+  float fm_demod_gain = static_cast<float>(phase1_channel_rate / (2.0 * pi * def_symbol_deviation));
   fm_demod = gr::analog::quadrature_demod_cf::make(fm_demod_gain);
-  
-  
-  // This is the original Approach
-  connect(self(), 0, pll_freq_lock, 0);
-  connect(pll_freq_lock, 0, pll_amp, 0);
-  connect(pll_amp, 0, noise_filter, 0);
-  connect(noise_filter, 0, sym_filter, 0);
-  connect(sym_filter, 0,fsk4_demod, 0);
-  connect(fsk4_demod, 0, self(), 0);
 
-/*
-  // This is the current approch in OP25, but they generate some of the filters differently
-  connect(self(), 0, fm_demod,0);
-  connect(fm_demod, 0, baseband_amp,0);
+  // Baseband Amplifier: unity gain (matches OP25 default)
+  baseband_amp = gr::blocks::multiply_const_ff::make(1.0f);
+
+  // C4FM Matched Filter
+  c4fm_taps = generate_c4fm_taps(phase1_channel_rate, phase1_symbol_rate, 9);
+  sym_filter = gr::filter::fir_filter_fff::make(1, c4fm_taps);
+
+  // FSK4 Demodulator: symbol timing recovery
+  fsk4_demod = gr::op25_repeater::fsk4_demod_ff::make(tune_queue, phase1_channel_rate, phase1_symbol_rate);
+
+  // Connect the chain
+  connect(self(), 0, fm_demod, 0);
+  connect(fm_demod, 0, baseband_amp, 0);
   connect(baseband_amp, 0, sym_filter, 0);
-  connect(sym_filter, 0,fsk4_demod, 0);
+  connect(sym_filter, 0, fsk4_demod, 0);
   connect(fsk4_demod, 0, self(), 0);
-*/
-  
 }
--- trunk-recorder/recorders/p25_recorder_fsk4_demod.h
+++ trunk-recorder/recorders/p25_recorder_fsk4_demod.h
@@ -2,17 +2,12 @@
 #define P25_RECORDER_FSK4_DEMOD_H
 
 #include <boost/shared_ptr.hpp>
-#include <gnuradio/analog/pll_freqdet_cf.h>
 #include <gnuradio/analog/quadrature_demod_cf.h>
 #include <gnuradio/block.h>
 #include <gnuradio/block_detail.h>
-#include <gnuradio/filter/fft_filter_fff.h>
-#include <gnuradio/filter/firdes.h>
 #include <gnuradio/hier_block2.h>
 #include <gnuradio/io_signature.h>
 #include <gnuradio/msg_queue.h>
-
-#include <gnuradio/filter/fft_filter_ccf.h>
 
 #if GNURADIO_VERSION < 0x030800
 #include <gnuradio/blocks/multiply_const_ff.h>
@@ -21,11 +16,7 @@
 #include <gnuradio/blocks/multiply_const.h>
 #include <gnuradio/filter/fir_filter_blk.h>
 #endif
-#include "../gr_blocks/rms_agc.h"
-#include <op25_repeater/fsk4_slicer_fb.h>
-#include <op25_repeater/rmsagc_ff.h>
 #include <op25_repeater/include/op25_repeater/fsk4_demod_ff.h>
-#include <gnuradio/digital/clock_recovery_mm_ff.h>
 
 class p25_recorder_fsk4_demod;
 
@@ -42,6 +33,7 @@ class p25_recorder_fsk4_demod : public gr::hier_block2 {
 
 protected:
   virtual void initialize();
+  std::vector<float> generate_c4fm_taps(double sample_rate, double symbol_rate, int span);
 
 public:
   p25_recorder_fsk4_demod();
@@ -51,20 +43,12 @@ public:
 private:
   const int phase1_samples_per_symbol = 5;
   const double phase1_symbol_rate = 4800;
-  std::vector<float> baseband_noise_filter_taps;
-  std::vector<float> sym_taps;
+  std::vector<float> c4fm_taps;
   gr::msg_queue::sptr tune_queue;
-  std::vector<float> cutoff_filter_coeffs;
-  gr::filter::fft_filter_fff::sptr noise_filter;
   gr::filter::fir_filter_fff::sptr sym_filter;
-  gr::filter::fft_filter_ccf::sptr cutoff_filter;
-  gr::blocks::multiply_const_ff::sptr pll_amp;
-  gr::analog::pll_freqdet_cf::sptr pll_freq_lock;
   gr::analog::quadrature_demod_cf::sptr fm_demod;
-  gr::op25_repeater::rmsagc_ff::sptr baseband_amp;
+  gr::blocks::multiply_const_ff::sptr baseband_amp;
   gr::op25_repeater::fsk4_demod_ff::sptr fsk4_demod;
-  gr::op25_repeater::fsk4_slicer_fb::sptr slicer;
-  gr::digital::clock_recovery_mm_ff::sptr clock_recovery;
-    void reset_block(gr::basic_block_sptr block); 
+  void reset_block(gr::basic_block_sptr block);
 };
 #endif
